78a3576b815ca5eb4e4ed3ceb8aed07c45742b6c
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-26 23:56:38 +0300

    Filled ApplicationEventHandler(handle ApplicationCreatedEvent)

--- src/main/java/com/example/authenticationserver/query/api/event/ApplicationEventHandler.java
+++ src/main/java/com/example/authenticationserver/query/api/event/ApplicationEventHandler.java
@@ -1,13 +1,25 @@
 package com.example.authenticationserver.query.api.event;
 
+import com.example.authenticationserver.query.api.data.ApplicationEntity;
+import com.example.authenticationserver.query.api.data.ApplicationRepository;
 import com.project.core.events.ApplicationCreatedEvent;
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationEventHandler {
+  private final ApplicationRepository applicationRepository;
+  @Autowired
+  public ApplicationEventHandler(ApplicationRepository applicationRepository) {
+    this.applicationRepository = applicationRepository;
+  }
+
   @EventHandler
   public void on(ApplicationCreatedEvent event){
-
+    ApplicationEntity applicationEntity = new ApplicationEntity();
+    BeanUtils.copyProperties(event,applicationEntity);
+    applicationRepository.save(applicationEntity);
   }
 }

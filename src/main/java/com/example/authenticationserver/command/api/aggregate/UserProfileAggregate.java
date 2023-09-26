b7b9b0cb7e47c00af50900308ab6e6b243bd40e1
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-26 09:28:46 +0300

    Added processing of the CreateUserProfileCommand

---
+++ src/main/java/com/example/authenticationserver/command/api/aggregate/UserProfileAggregate.java
@@ -0,0 +1,46 @@
+package com.example.authenticationserver.command.api.aggregate;
+
+import com.project.core.commands.CreateUserProfileCommand;
+import com.project.core.events.UserProfileCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import java.util.Date;
+
+@Aggregate
+public class UserProfileAggregate {
+  @AggregateIdentifier
+  private String userId;
+  private String userName;
+  private String passwordHash;
+  private String userStatus;
+  private Date createdAt;
+  private Date lastUpdatedAt;
+  private Date deleteAt;
+
+  public UserProfileAggregate() {
+  }
+  @CommandHandler
+  public UserProfileAggregate(CreateUserProfileCommand command){
+    UserProfileCreatedEvent event =
+      UserProfileCreatedEvent.builder()
+        .userId(command.getUserId())
+        .userName(command.getUserName())
+        .passwordHash(command.getPasswordHash())
+        .userStatus(command.getUserStatus())
+        .createdAt(command.getCreatedAt())
+        .build();
+    AggregateLifecycle.apply(event);
+  }
+  @EventSourcingHandler
+  public void on(UserProfileCreatedEvent event){
+    this.userId = event.getUserId();
+    this.userName = event.getUserName();
+    this.passwordHash = event.getPasswordHash();
+    this.userStatus = event.getUserStatus();
+    this.createdAt = event.getCreatedAt();
+  }
+}

aec32faa521dbb8bc93d33de062f2d04a0762066
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-26 09:53:48 +0300

    Commands and events are added to the trusted package in XStream

--- src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
+++ src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
@@ -1,13 +1,25 @@
 package com.example.authenticationserver;
 
+import com.project.core.commands.CreateUserProfileCommand;
+import com.project.core.events.UserProfileCreatedEvent;
+import com.thoughtworks.xstream.XStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthenticationServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AuthenticationServerApplication.class, args);
+  }
+
+  @Bean
+  public XStream xStream() {
+    XStream xStream = new XStream();
+    xStream.allowTypeHierarchy(CreateUserProfileCommand.class);
+    xStream.allowTypeHierarchy(UserProfileCreatedEvent.class);
+    return xStream;
   }
 
 }

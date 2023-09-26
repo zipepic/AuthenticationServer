47b4a8faf528dc71394a6d7cc1b3331d47c935c0
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-26 19:06:03 +0300

    Added bean BCryptPasswordEncoder

--- src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
+++ src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
@@ -6,6 +6,8 @@ import com.thoughtworks.xstream.XStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AuthenticationServerApplication {
@@ -21,5 +23,8 @@ public class AuthenticationServerApplication {
     xStream.allowTypeHierarchy(UserProfileCreatedEvent.class);
     return xStream;
   }
-
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }

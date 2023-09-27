7a8cf37fe3aacdbb2bc88e74f250c864749070a9
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-27 19:03:55 +0300

    Added processing of the ApplicationRegisteredEvent event

--- src/main/java/com/example/authenticationserver/query/api/event/ApplicationEventHandler.java
+++ src/main/java/com/example/authenticationserver/query/api/event/ApplicationEventHandler.java
@@ -3,10 +3,13 @@ package com.example.authenticationserver.query.api.event;
 import com.example.authenticationserver.query.api.data.ApplicationEntity;
 import com.example.authenticationserver.query.api.data.ApplicationRepository;
 import com.project.core.events.ApplicationCreatedEvent;
+import com.project.core.events.ApplicationRegisteredEvent;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class ApplicationEventHandler {
@@ -21,5 +24,17 @@ public class ApplicationEventHandler {
     ApplicationEntity applicationEntity = new ApplicationEntity();
     BeanUtils.copyProperties(event,applicationEntity);
     applicationRepository.save(applicationEntity);
+  }
+  @EventHandler
+  public void handle(ApplicationRegisteredEvent event){
+    Optional<ApplicationEntity> optionalApplicationEntity =
+      applicationRepository.findById(event.getClientId());
+    if(optionalApplicationEntity.isPresent()){
+      ApplicationEntity applicationEntity = optionalApplicationEntity.get();
+      applicationEntity.setCode(event.getCode());
+      applicationRepository.save(applicationEntity);
+    }else {
+      throw new IllegalArgumentException("Not found");
+    }
   }
 }

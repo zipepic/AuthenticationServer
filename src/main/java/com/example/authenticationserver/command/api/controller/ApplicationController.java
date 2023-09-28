8c5afe786974ccac89eb0eb973efc36f12d2c3ee
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-28 21:01:14 +0300

    Added encode password

--- src/main/java/com/example/authenticationserver/command/api/controller/ApplicationController.java
+++ src/main/java/com/example/authenticationserver/command/api/controller/ApplicationController.java
@@ -8,6 +8,7 @@ import com.project.core.commands.LoginApplicationCommand;
 import com.project.core.commands.RegisterApplicationCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/app")
 public class ApplicationController {
   private final CommandGateway commandGateway;
+  private final PasswordEncoder passwordEncoder;
   @Autowired
-  public ApplicationController(CommandGateway commandGateway) {
+  public ApplicationController(CommandGateway commandGateway, PasswordEncoder passwordEncoder) {
     this.commandGateway = commandGateway;
+    this.passwordEncoder = passwordEncoder;
   }
   @PostMapping("/register")
   public String register(@RequestBody ApplicationRegistrationRestModel model){
@@ -39,7 +42,7 @@ public class ApplicationController {
     LoginApplicationCommand command =
       LoginApplicationCommand.builder()
         .clientId(model.getClientId())
-        .secret(model.getSecret())
+        .secret(passwordEncoder.encode(model.getSecret()))
         .grantType(model.getGrantType())
         .code(model.getCode())
         .redirectUrl(model.getRedirectUrl())
@@ -52,7 +55,7 @@ public class ApplicationController {
     CreateApplicationCommand command =
       CreateApplicationCommand.builder()
         .clientId(model.getClientId())
-        .secret(model.getSecret())
+        .secret(passwordEncoder.encode(model.getSecret()))
         .build();
     return commandGateway.sendAndWait(command);
   }

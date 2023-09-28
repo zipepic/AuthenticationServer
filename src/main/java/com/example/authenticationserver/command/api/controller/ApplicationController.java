ede8edd723ba2f3ea9f358e38788c181ef06ce3b
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-28 11:55:47 +0300

    Fill endpoint

--- src/main/java/com/example/authenticationserver/command/api/controller/ApplicationController.java
+++ src/main/java/com/example/authenticationserver/command/api/controller/ApplicationController.java
@@ -2,6 +2,10 @@ package com.example.authenticationserver.command.api.controller;
 
 import com.example.authenticationserver.command.api.restmodel.ApplicationLoginRestModel;
 import com.example.authenticationserver.command.api.restmodel.ApplicationRegistrationRestModel;
+import com.project.core.commands.LoginApplicationCommand;
+import com.project.core.commands.RegisterApplicationCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/app")
 public class ApplicationController {
+  private final CommandGateway commandGateway;
+  @Autowired
+  public ApplicationController(CommandGateway commandGateway) {
+    this.commandGateway = commandGateway;
+  }
   @PostMapping("/register")
   public String register(@RequestBody ApplicationRegistrationRestModel model){
-    return "";
+    RegisterApplicationCommand command =
+      RegisterApplicationCommand.builder()
+        .clientId(model.getClientId())
+        .responseType(model.getResponseType())
+        .state(model.getState())
+        .scope(model.getScope())
+        .redirectUrl(model.getRedirectUrl())
+        .build();
+
+    return commandGateway.sendAndWait(command);
   }
   @PostMapping("/login")
   public String login(@RequestBody ApplicationLoginRestModel model){
-    return "";
+    LoginApplicationCommand command =
+      LoginApplicationCommand.builder()
+        .clientId(model.getClientId())
+        .secret(model.getSecret())
+        .grantType(model.getGrantType())
+        .code(model.getCode())
+        .redirectUrl(model.getRedirectUrl())
+        .build();
+
+    return commandGateway.sendAndWait(command);
   }
 }

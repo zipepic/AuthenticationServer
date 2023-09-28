d262c3c086fc506266720b506bfba7245a9ef385
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-28 11:10:57 +0300

    Fix Spring Security, added config for headers

--- src/main/java/com/example/authenticationserver/config/SecurityConfig.java
+++ src/main/java/com/example/authenticationserver/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-//package com.example.authenticationserver.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import java.util.Arrays;
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//      http.csrf(csrf -> csrf.disable())
-//        .formLogin(formLogin -> formLogin.disable())
-//        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//        .authorizeHttpRequests(auth ->
-//          auth
-//            .anyRequest().permitAll()
-//        );
-//        return http.build();
-//    }
-//}
-//
+package com.example.authenticationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      http.csrf(csrf -> csrf.disable())
+        .formLogin(formLogin -> formLogin.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+        .authorizeHttpRequests(auth ->
+          auth
+            .anyRequest().permitAll()
+        );
+        return http.build();
+    }
+}
+

203ec5319cea17d31e0237697562f2bef1f90083
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-29 23:06:48 +0300

    New fields added and old ones renamed

--- src/main/java/com/example/authenticationserver/command/api/restmodel/TokenSummary.java
+++ src/main/java/com/example/authenticationserver/command/api/restmodel/TokenSummary.java
@@ -6,6 +6,13 @@ import lombok.Data;
 @Data
 @Builder
 public class TokenSummary {
-  private String accessToken;
-  private String refreshToken;
+  private String access_token;
+  private Integer expires_in;
+  private Integer refresh_expires_in;
+  private String refresh_token;
+  private String token_type;
+  private String id_token;
+  private String not_before_policy;
+  private String session_state;
+  private String scope;
 }

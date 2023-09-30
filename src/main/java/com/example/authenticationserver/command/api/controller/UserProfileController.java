d68e75789166bca7f18dd8b7d62f3400e95aa9b4
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-30 21:58:44 +0300

    Added processing of the token issuance process

--- src/main/java/com/example/authenticationserver/command/api/controller/UserProfileController.java
+++ src/main/java/com/example/authenticationserver/command/api/controller/UserProfileController.java
@@ -1,38 +1,57 @@
 package com.example.authenticationserver.command.api.controller;
 
-import com.example.authenticationserver.command.api.restmodel.user.UserProfileRestModel;
-import com.project.core.commands.CreateUserProfileCommand;
+import com.example.authenticationserver.command.api.restmodel.TokenSummary;
+import com.project.core.commands.UseOneTimeCodeCommand;
+import com.project.core.queries.CheckLoginDataQuery;
+import com.project.core.queries.FindUserIdByOneTimeCodeQuery;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
 public class UserProfileController {
   private final CommandGateway commandGateway;
+  private final QueryGateway queryGateway;
+  private final PasswordEncoder passwordEncoder;
 
   @Autowired
-  public UserProfileController(CommandGateway commandGateway) {
+  public UserProfileController(CommandGateway commandGateway, QueryGateway queryGateway, PasswordEncoder passwordEncoder) {
     this.commandGateway = commandGateway;
+    this.queryGateway = queryGateway;
+    this.passwordEncoder = passwordEncoder;
   }
 
-  @PostMapping
-  public String create(@RequestBody UserProfileRestModel restModel) {
-    UUID uuid = UUID.randomUUID();
-    CreateUserProfileCommand command = buildCreateUserProfileCommand(restModel, uuid);
+  @PostMapping("/token")
+  public TokenSummary generateTokens(@RequestParam String grant_type,
+                                     @RequestParam String client_id,
+                                     @RequestParam String client_secret,
+                                     @RequestParam String code,
+                                     @RequestParam String redirect_uri){
+    CheckLoginDataQuery loginDataQuery =
+      CheckLoginDataQuery.builder()
+        .clientId(client_id)
+        .secret(client_secret)
+        .build();
+    boolean applicationIsPresent = queryGateway.query(loginDataQuery, Boolean.class).join();
+    if(!applicationIsPresent)
+      throw new RuntimeException("Application is not present");
+
+    FindUserIdByOneTimeCodeQuery findUserQuery =
+      FindUserIdByOneTimeCodeQuery.builder()
+        .code(code)
+        .build();
+    String userId = queryGateway.query(findUserQuery, String.class).join();
+
+    UseOneTimeCodeCommand command =
+      UseOneTimeCodeCommand.builder()
+        .userId(userId)
+        .code(code)
+        .build();
+
     return commandGateway.sendAndWait(command);
   }
 
-  private CreateUserProfileCommand buildCreateUserProfileCommand(UserProfileRestModel restModel, UUID uuid) {
-    return CreateUserProfileCommand.builder()
-      .userId(uuid.toString())
-      .userName(restModel.getUserName())
-      .passwordHash(restModel.getPassword())
-      .userStatus("CREATE")
-      .createdAt(new Date())
-      .build();
   }
-}

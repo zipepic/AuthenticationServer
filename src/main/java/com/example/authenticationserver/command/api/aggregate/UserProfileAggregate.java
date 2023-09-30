2de8b01d767783ebf3c973e51f6cc7a3c2821205
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-30 22:00:05 +0300

    Added a new field (code) and also added the implementation of creating a one-time code and issuing tokens (only a stub for now)

--- src/main/java/com/example/authenticationserver/command/api/aggregate/UserProfileAggregate.java
+++ src/main/java/com/example/authenticationserver/command/api/aggregate/UserProfileAggregate.java
@@ -1,25 +1,37 @@
 package com.example.authenticationserver.command.api.aggregate;
 
+import com.example.authenticationserver.command.api.restmodel.TokenSummary;
 import com.project.core.commands.CreateUserProfileCommand;
+import com.project.core.commands.GenerateOneTimeCodeUserProfileCommand;
+import com.project.core.commands.UseOneTimeCodeCommand;
+import com.project.core.events.ApplicationRegisteredEvent;
+import com.project.core.events.OneTimeCodeUserProfileGeneratedEvent;
 import com.project.core.events.UserProfileCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
-
+import java.util.UUID;
+@Slf4j
 @Aggregate
 public class UserProfileAggregate {
   @AggregateIdentifier
   private String userId;
   private String userName;
   private String passwordHash;
+  private String code;
   private String userStatus;
   private Date createdAt;
   private Date lastUpdatedAt;
   private Date deleteAt;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   public UserProfileAggregate() {
   }
@@ -42,5 +54,47 @@ public class UserProfileAggregate {
     this.passwordHash = event.getPasswordHash();
     this.userStatus = event.getUserStatus();
     this.createdAt = event.getCreatedAt();
+  }
+  @CommandHandler
+  public String handle(GenerateOneTimeCodeUserProfileCommand command){
+    if(!passwordEncoder.matches(command.getPasswordHash(), this.passwordHash)){
+      throw new IllegalArgumentException("Password is not correct");
+    }
+    //TODO implement code generation with JWT
+    String code = UUID.randomUUID().toString();
+
+    log.info("Code -> {}", code);
+
+    OneTimeCodeUserProfileGeneratedEvent event = OneTimeCodeUserProfileGeneratedEvent.builder()
+      .userId(command.getUserId())
+      .code(code)
+      .build();
+    AggregateLifecycle.apply(event);
+    return code;
+  }
+  @EventSourcingHandler
+  public void on(OneTimeCodeUserProfileGeneratedEvent event){
+    this.code = event.getCode();
+  }
+
+  @CommandHandler
+  public TokenSummary handle(UseOneTimeCodeCommand command){
+    OneTimeCodeUserProfileGeneratedEvent event = OneTimeCodeUserProfileGeneratedEvent.builder()
+      .userId(command.getUserId())
+      .code(null)
+      .build();
+    AggregateLifecycle.apply(event);
+    //TODO implement token generation
+    return TokenSummary.builder()
+      .access_token("access_token")
+      .expires_in(60000)
+      .refresh_expires_in(60000*7)
+      .refresh_token("refresh_token")
+      .token_type("bearer")
+      .id_token("id_token")
+      .not_before_policy("not_before_policy")
+      .session_state("session_state")
+      .scope("scope")
+      .build();
   }
 }

21e2497c8cf9e99d99f2cff3fb7f035313b98ede
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-30 21:57:59 +0300

    Added processing of the registration process

--- src/main/java/com/example/authenticationserver/command/api/controller/UserViewController.java
+++ src/main/java/com/example/authenticationserver/command/api/controller/UserViewController.java
@@ -1,23 +1,30 @@
 package com.example.authenticationserver.command.api.controller;
 
-import com.project.core.commands.CreateUserProfileCommand;
+import com.project.core.commands.GenerateOneTimeCodeUserProfileCommand;
+import com.project.core.queries.FindUserIdByUserNameQuery;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @Slf4j
 @Controller
 @RequestMapping("/login")
 public class UserViewController {
-
+  private final QueryGateway queryGateway;
+  private final PasswordEncoder passwordEncoder;
+  private final CommandGateway commandGateway;
+  @Autowired
+  public UserViewController(QueryGateway queryGateway, PasswordEncoder passwordEncoder, CommandGateway commandGateway) {
+    this.queryGateway = queryGateway;
+    this.passwordEncoder = passwordEncoder;
+    this.commandGateway = commandGateway;
+  }
 
   @GetMapping
   public String showLoginForm(@RequestParam("client_id") String clientId,
@@ -43,7 +50,21 @@ public class UserViewController {
                       @RequestParam("scope") String scope,
                       @RequestParam("redirect_url") String redirectUrl,
                       HttpServletResponse response) {
-    String code = UUID.randomUUID().toString();
+    FindUserIdByUserNameQuery query =
+      FindUserIdByUserNameQuery.builder()
+        .userName(username)
+        .build();
+
+    String userId = queryGateway.query(query, String.class).join();
+
+    GenerateOneTimeCodeUserProfileCommand command =
+      GenerateOneTimeCodeUserProfileCommand.builder()
+        .userId(userId)
+        .passwordHash(password)
+        .build();
+
+    String code = commandGateway.sendAndWait(command);
+
     if (isValidUser(username, password)) {
       return "redirect:" + redirectUrl + "?state=" + state + "&code=" + code;
     } else {

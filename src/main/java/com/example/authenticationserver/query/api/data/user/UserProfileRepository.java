48c445b02e0e3c64328e0ebed3a122f6bf359e66
zipepic <olezhka.kirilov.97@mail.ru>
2023-09-30 21:55:47 +0300

    New methods have been added to search for id

--- src/main/java/com/example/authenticationserver/query/api/data/user/UserProfileRepository.java
+++ src/main/java/com/example/authenticationserver/query/api/data/user/UserProfileRepository.java
@@ -2,5 +2,9 @@ package com.example.authenticationserver.query.api.data.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, String> {
+  public Optional<UserProfileEntity> findByUserName(String userName);
+  public Optional<UserProfileEntity> findByCode(String code);
 }

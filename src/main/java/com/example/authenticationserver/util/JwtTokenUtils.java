007f445293851f7d88da807d11a08c0204b83754
zipepic <olezhka.kirilov.97@mail.ru>
2023-10-08 19:45:12 +0300

    Added new methods for updating tokens and methods for conveniently creating a new token

--- src/main/java/com/example/authenticationserver/util/JwtTokenUtils.java
+++ src/main/java/com/example/authenticationserver/util/JwtTokenUtils.java
@@ -1,18 +1,20 @@
 package com.example.authenticationserver.util;
+import com.example.authenticationserver.query.api.dto.TokenAuthorizationCodeDTO;
 import com.project.core.commands.ResourceServerDTO;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.time.Duration;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 @Slf4j
 public class JwtTokenUtils {
+  //TODO refracting this
   private static Key secretKey;
 
   public JwtTokenUtils(SecretKeySpec secretKey) {
@@ -29,7 +31,44 @@ public class JwtTokenUtils {
       .signWith(secretKey);
     return jwt.compact();
   }
-
+  public static String lightGenerateToken(JwtBuilder jwt) {
+    return jwt.signWith(secretKey).compact();
+  }
+  public static Claims extractClaims(String token) {
+    try {
+      return Jwts.parser()
+        .setSigningKey(secretKey)
+        .parseClaimsJws(token)
+        .getBody();
+    } catch (JwtException e) {
+      throw new IllegalArgumentException("Invalid token");
+    }
+  }
+  public static String lightGenerateTokenWithClaims(Claims claims){
+    return Jwts.builder()
+      .setClaims(claims)
+      .signWith(secretKey)
+      .compact();
+  }
+  public static TokenAuthorizationCodeDTO refresh(String token) {
+    Claims claims = extractClaims(token);
+    long expirationRefreshToken = Duration.between(claims.getIssuedAt().toInstant(),
+        claims.getExpiration().toInstant())
+      .toMillis();
+    long exprirationAccessToken = expirationRefreshToken/10;
+    claims.setIssuedAt(new Date());
+    Claims refreshTokenClaims = claims
+      .setExpiration(new Date(System.currentTimeMillis() + expirationRefreshToken));
+    Claims accessTokenClaims = claims
+      .setExpiration(new Date(System.currentTimeMillis() + exprirationAccessToken));
+    return TokenAuthorizationCodeDTO.builder()
+      .accessToken(lightGenerateTokenWithClaims(accessTokenClaims))
+      .expiresIn((int) exprirationAccessToken)
+      .refreshExpiresIn((int) expirationRefreshToken)
+      .refreshToken(lightGenerateTokenWithClaims(refreshTokenClaims))
+      .tokenType(claims.get("type",String.class))
+      .build();
+  }
   public static boolean validateToken(String token) {
     try {
       Jwts.parser()
@@ -54,6 +93,7 @@ public class JwtTokenUtils {
 
     return tokens;
   }
+
 }
 
 

ffb16a43f7aac5e40cc3944b8c6d25c5ad6b0f3f
zipepic <olezhka.kirilov.97@mail.ru>
2023-10-10 22:40:00 +0300

    Separating filter chains for OAuth and internal management

--- src/main/java/com/example/authenticationserver/config/SecurityConfig.java
+++ src/main/java/com/example/authenticationserver/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.authenticationserver.config;
 
 import com.example.authenticationserver.security.AuthUserProfileProviderImpl;
 import com.example.authenticationserver.service.UserProfileDetailsService;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,14 +25,17 @@ public class SecurityConfig {
 
   private final AuthUserProfileProviderImpl authUserProfileProvider;
   private final UserProfileDetailsService userProfileDetailsService;
+  private final QueryGateway queryGateway;
   @Autowired
-  public SecurityConfig(AuthUserProfileProviderImpl authUserProfileProvider, UserProfileDetailsService userProfileDetailsService) {
+  public SecurityConfig(AuthUserProfileProviderImpl authUserProfileProvider, UserProfileDetailsService userProfileDetailsService, QueryGateway queryGateway) {
     this.authUserProfileProvider = authUserProfileProvider;
     this.userProfileDetailsService = userProfileDetailsService;
+    this.queryGateway = queryGateway;
   }
 
   @Bean
   protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.securityMatcher(new AntPathRequestMatcher("/security/**"));
     http.csrf(AbstractHttpConfigurer::disable);
     http.formLogin(formLogin -> formLogin.disable());
     http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
@@ -41,14 +45,42 @@ public class SecurityConfig {
 //          .requestMatchers(new AntPathRequestMatcher("/security/**")).hasRole("USER")
           .anyRequest().permitAll())
       .httpBasic(Customizer.withDefaults());
-    http.addFilterBefore(new JwtFilter(userProfileDetailsService), UsernamePasswordAuthenticationFilter.class);
+    http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    http.authenticationProvider(authUserProfileProvider);
+    return http.build();
+  }
+  @Bean
+  protected SecurityFilterChain filterChainRefresh(HttpSecurity http) throws Exception {
+    http.securityMatcher(new AntPathRequestMatcher("/auth/**"));
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.formLogin(formLogin -> formLogin.disable());
+    http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.authorizeHttpRequests(authorize ->
+        authorize
+          .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
+          .anyRequest().authenticated())
+      .httpBasic(Customizer.withDefaults());
+
+    http.addFilterBefore(new TokenSignatureVerificationFilter(), UsernamePasswordAuthenticationFilter.class);
+    http.addFilterAfter(new LoadUserFromDatabaseFilterByJwt(userProfileDetailsService), TokenSignatureVerificationFilter.class);
+    http.addFilterAfter(jwtRefreshFilter(), LoadUserFromDatabaseFilterByJwt.class);
+
     http.authenticationProvider(authUserProfileProvider);
     return http.build();
   }
   @Bean
   public WebSecurityCustomizer webSecurityCustomizer() {
-    return (web) -> web.ignoring().
-      requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    return (web) -> web.ignoring()
+      .requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+      .requestMatchers(new AntPathRequestMatcher("/auth/login"));
+  }
+  @Bean
+  JwtFilter jwtFilter(){
+    return new JwtFilter(userProfileDetailsService,queryGateway);
+  }
+  JwtRefreshFilter jwtRefreshFilter(){
+    return new JwtRefreshFilter(queryGateway);
   }
 }
 

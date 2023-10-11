47f70f0a1db42e3677869c197dd4a835992233fc
zipepic <olezhka.kirilov.97@mail.ru>
2023-10-11 17:17:35 +0300

    The order of operation of the filters is determined

--- src/main/java/com/example/authenticationserver/config/SecurityConfig.java
+++ src/main/java/com/example/authenticationserver/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.example.authenticationserver.config;
 
+import com.example.authenticationserver.config.filter.ErrorHandlingFilter;
+import com.example.authenticationserver.config.filter.auth.LoadUserFromDatabaseFilterByJwt;
+import com.example.authenticationserver.config.filter.auth.TokenSignatureVerificationFilter;
+import com.example.authenticationserver.config.filter.token.JwtRefreshFilter;
+import com.example.authenticationserver.config.filter.token.TokenGenerationFilter;
+import com.example.authenticationserver.config.filter.token.TokenTypeFilter;
 import com.example.authenticationserver.security.AuthUserProfileProviderImpl;
 import com.example.authenticationserver.service.UserProfileDetailsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,61 +34,48 @@ public class SecurityConfig {
   private final AuthUserProfileProviderImpl authUserProfileProvider;
   private final UserProfileDetailsService userProfileDetailsService;
   private final QueryGateway queryGateway;
+  private final CommandGateway commandGateway;
   @Autowired
-  public SecurityConfig(AuthUserProfileProviderImpl authUserProfileProvider, UserProfileDetailsService userProfileDetailsService, QueryGateway queryGateway) {
+  public SecurityConfig(AuthUserProfileProviderImpl authUserProfileProvider, UserProfileDetailsService userProfileDetailsService, QueryGateway queryGateway, CommandGateway commandGateway) {
     this.authUserProfileProvider = authUserProfileProvider;
     this.userProfileDetailsService = userProfileDetailsService;
     this.queryGateway = queryGateway;
-  }
-
-  @Bean
-  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.securityMatcher(new AntPathRequestMatcher("/security/**"));
-    http.csrf(AbstractHttpConfigurer::disable);
-    http.formLogin(formLogin -> formLogin.disable());
-    http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
-    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    http.authorizeHttpRequests(authorize ->
-        authorize
-//          .requestMatchers(new AntPathRequestMatcher("/security/**")).hasRole("USER")
-          .anyRequest().permitAll())
-      .httpBasic(Customizer.withDefaults());
-    http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
-    http.authenticationProvider(authUserProfileProvider);
-    return http.build();
+    this.commandGateway = commandGateway;
   }
   @Bean
-  protected SecurityFilterChain filterChainRefresh(HttpSecurity http) throws Exception {
+  @Order(1)
+  protected SecurityFilterChain filterChainAuth(HttpSecurity http) throws Exception {
     http.securityMatcher(new AntPathRequestMatcher("/auth/**"));
     http.csrf(AbstractHttpConfigurer::disable);
     http.formLogin(formLogin -> formLogin.disable());
     http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
     http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    http.authorizeHttpRequests(authorize ->
-        authorize
-          .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
-          .anyRequest().authenticated())
-      .httpBasic(Customizer.withDefaults());
-
-    http.addFilterBefore(new TokenSignatureVerificationFilter(), UsernamePasswordAuthenticationFilter.class);
+    http.addFilterBefore(errorHandlingFilter(), UsernamePasswordAuthenticationFilter.class);
+    http.addFilterAfter(new TokenSignatureVerificationFilter(), ErrorHandlingFilter.class);
     http.addFilterAfter(new LoadUserFromDatabaseFilterByJwt(userProfileDetailsService), TokenSignatureVerificationFilter.class);
-    http.addFilterAfter(jwtRefreshFilter(), LoadUserFromDatabaseFilterByJwt.class);
 
     http.authenticationProvider(authUserProfileProvider);
     return http.build();
   }
+
   @Bean
   public WebSecurityCustomizer webSecurityCustomizer() {
     return (web) -> web.ignoring()
       .requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
       .requestMatchers(new AntPathRequestMatcher("/auth/login"));
   }
-  @Bean
-  JwtFilter jwtFilter(){
-    return new JwtFilter(userProfileDetailsService,queryGateway);
+
+  TokenTypeFilter tokenTypeFilter(){
+    return new TokenTypeFilter(jwtRefreshFilter());
   }
   JwtRefreshFilter jwtRefreshFilter(){
-    return new JwtRefreshFilter(queryGateway);
+    return new JwtRefreshFilter(tokenGenerationFilter());
+  }
+  TokenGenerationFilter tokenGenerationFilter(){
+    return new TokenGenerationFilter(commandGateway, new ObjectMapper());
+  }
+  ErrorHandlingFilter errorHandlingFilter(){
+    return new ErrorHandlingFilter(new ObjectMapper());
   }
 }
 

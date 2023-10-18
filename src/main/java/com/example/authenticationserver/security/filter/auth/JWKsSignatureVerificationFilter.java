7cabe2c5efb485234b639ec35dc2e4324dba7ab8
zipepic <olezhka.kirilov.97@mail.ru>
2023-10-18 17:40:25 +0300

    Complete

--- src/main/java/com/example/authenticationserver/security/filter/auth/JWKsSignatureVerificationFilter.java
+++ src/main/java/com/example/authenticationserver/security/filter/auth/JWKsSignatureVerificationFilter.java
@@ -1,14 +1,20 @@
 package com.example.authenticationserver.security.filter.auth;
 
 import com.example.authenticationserver.test.JwkManager;
+import com.nimbusds.jose.jwk.JWKSet;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class JWKsSignatureVerificationFilter extends OncePerRequestFilter {
   private final JwkManager jwtTokenGenerator;
@@ -27,15 +33,15 @@ public class JWKsSignatureVerificationFilter extends OncePerRequestFilter {
     authToken = authToken.replace("Bearer ", "");
     try {
       var claims = jwtTokenGenerator.verifyAndParseJWT(authToken);
+
+      if(!(SecurityContextHolder.getContext().getAuthentication() == null))
+        throw new IllegalArgumentException("Security context already contains an authentication object");
+
+      Authentication authentication = new UsernamePasswordAuthenticationToken(null,claims,null);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
     } catch (Exception e) {
       throw new IllegalArgumentException("Invalid token");
     }
-
-    if(!(SecurityContextHolder.getContext().getAuthentication() == null))
-      throw new IllegalArgumentException("Security context already contains an authentication object");
-
-
-
     filterChain.doFilter(request, response);
   }
 }

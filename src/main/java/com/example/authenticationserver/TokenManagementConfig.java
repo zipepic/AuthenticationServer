132653efc2c8367dbd31a89a7515d35b1cdd7cc5
zipepic <olezhka.kirilov.97@mail.ru>
2023-11-13 11:39:17 +0300

    Refactored

--- src/main/java/com/example/authenticationserver/TokenManagementConfig.java
+++ src/main/java/com/example/authenticationserver/TokenManagementConfig.java
@@ -1,12 +1,10 @@
 package com.example.authenticationserver;
-import com.nimbusds.jose.jwk.JWK;
+import com.project.core.events.user.JwkTokenInfoEvent;
 import com.project.core.queries.user.FetchJwkSet;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
-import org.axonframework.queryhandling.GenericQueryMessage;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import tokenlib.util.TokenFacade;
-import tokenlib.util.TokenProcessorFactory;
+import tokenlib.util.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +13,10 @@ import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import tokenlib.util.jwk.SimpleJWK;
 
-import java.util.List;
-
 
 @Configuration
 @ManagedResource(description = "Token Processor Factory", objectName = "com.example.authenticationserver:type=TokenProcessorFactory")
-public class AppConfig {
+public class TokenManagementConfig {
 
   @Value("${app.secret}")
   private String jwtSecret;
@@ -28,13 +24,13 @@ public class AppConfig {
   private int tokenTypeInt;
   private final QueryGateway queryGateway;
   @Autowired
-  public AppConfig(QueryGateway queryGateway) {
+  public TokenManagementConfig(QueryGateway queryGateway) {
     this.queryGateway = queryGateway;
   }
 
   @ManagedAttribute(description = "Set Token Type. 0 - JWT, 1 - JWK")
   public void setTokenTypeInt(int tokenTypeInt) {
-    var context = new AnnotationConfigApplicationContext(AppConfig.class);
+    var context = new AnnotationConfigApplicationContext(TokenManagementConfig.class);
     if (tokenTypeInt < 0 || tokenTypeInt > 1)
       throw new IllegalArgumentException("Unknown token type");
     this.tokenTypeInt = tokenTypeInt;
@@ -46,8 +42,8 @@ public class AppConfig {
   }
 
   @Bean
-  public TokenProcessorFactory tokenProcessorFactory() {
-    return new TokenProcessorFactory(queryGateway, (x->{
+  public AbstractTokenFactory tokenProcessorFactory() {
+    var factory = new TokenProcessorFactory(jwtSecret,queryGateway, (x->{
 
 //      GenericQueryMessage<String, List<JWK>> query
 //        = new GenericQueryMessage<>("fetchJwkSet",
@@ -56,10 +52,13 @@ public class AppConfig {
 
       return queryGateway.query(query,ResponseTypes.multipleInstancesOf(SimpleJWK.class)).join();
     }));
+    factory.setEventClassForJwk(JwkTokenInfoEvent.class);
+    factory.setEventClassForJwt(JwkTokenInfoEvent.class);
+    return factory;
   }
 
   @Bean
-  public TokenFacade tokenFacade(TokenProcessorFactory tokenProcessorFactory) {
-      return tokenProcessorFactory.generate(tokenTypeInt);
+  public TokenFacade tokenFacade(AbstractTokenFactory abstractTokenFactory) {
+      return abstractTokenFactory.generate(tokenTypeInt);
   }
 }

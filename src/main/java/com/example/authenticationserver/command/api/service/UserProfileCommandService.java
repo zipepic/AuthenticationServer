64be5260155ae50dbe7f7cd33a7201326bbdda06
zipepic <olezhka.kirilov.97@mail.ru>
2023-11-21 23:48:09 +0300

    Complete TODO mark in UserProfileCommandService

--- src/main/java/com/example/authenticationserver/command/api/service/UserProfileCommandService.java
+++ src/main/java/com/example/authenticationserver/command/api/service/UserProfileCommandService.java
@@ -5,7 +5,6 @@ import com.example.authenticationserver.security.UserProfileDetails;
 import com.project.core.commands.user.CreateUserProfileCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,7 +26,6 @@ public class UserProfileCommandService {
   }
   public CompletableFuture<Object> register(Authentication authentication){
     UUID uuid = UUID.randomUUID();
-    //TODO validate username
     CreateUserProfileCommand command =
       CreateUserProfileCommand.builder()
         .userId(uuid.toString())

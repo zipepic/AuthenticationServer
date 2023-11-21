a5ae5366b1ad116b94d1cec68ca34f6935b68af1
zipepic <olezhka.kirilov.97@mail.ru>
2023-11-21 23:47:42 +0300

    Refactored

--- src/main/java/com/example/authenticationserver/security/service/UserProfileDetailsLoaderService.java
+++ src/main/java/com/example/authenticationserver/security/service/UserProfileDetailsLoaderService.java
@@ -7,7 +7,6 @@ import com.project.core.queries.user.FetchUserProfileByUserNameQuery;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 

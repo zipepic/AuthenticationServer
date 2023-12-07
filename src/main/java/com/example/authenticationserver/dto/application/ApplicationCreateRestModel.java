835e33542de73ccb3dfbc20bcfb610800b9e4496
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-07 20:13:45 +0300

    Moved

--- src/main/java/com/example/authenticationserver/dto/application/ApplicationCreateRestModel.java
+++
@@ -1,9 +0,0 @@
-package com.example.authenticationserver.dto.application;
-
-import lombok.Data;
-
-@Data
-public class ApplicationCreateRestModel {
-  private String clientId;
-  private String secret;
-}
